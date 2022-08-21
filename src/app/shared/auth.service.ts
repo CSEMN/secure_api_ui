7957ab595e2fea393a4a763a8f1fbbed63be4187
Mahmoud Nasser <csemn.mail@gmail.com>
2022-08-21 19:49:13 +0200

    online db
    added online db routs

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -15,20 +15,20 @@ export class AuthService {
   constructor(private http: HttpClient) {}
   // User registration
   register(user: User): Observable<any> {
-    return this.http.post('http://127.0.0.1:8000/api/register', user);
+    return this.http.post('https://secure-api-jwt.herokuapp.com/api/register', user);
   }
   // Login
   signin(user: User): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/login', user);
+    return this.http.post<any>('https://secure-api-jwt.herokuapp.com/api/login', user);
   }
   google_signin(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:8000/api/google/redirect');
+    return this.http.get<any>('https://secure-api-jwt.herokuapp.com/api/google/redirect');
   }
   github_signin(): Observable<any> {
-    return this.http.get<any>('http://127.0.0.1:8000/api/github/redirect');
+    return this.http.get<any>('https://secure-api-jwt.herokuapp.com/api/github/redirect');
   }
   // Access user profile
   profileUser(): Observable<any> {
-    return this.http.get('http://127.0.0.1:8000/api/profile');
+    return this.http.get('https://secure-api-jwt.herokuapp.com/api/profile');
   }
 }
