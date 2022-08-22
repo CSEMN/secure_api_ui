95e46301dc7dc9ffd6ee5c1791cb7d92d8f6b5e0
Mahmoud Nasser <csemn.mail@gmail.com>
2022-08-22 17:26:23 +0200

    google login
    added google login feature

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnInit, QueryList, ViewChildren} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TokenService } from '../../shared/token.service';
 import { AuthStateService } from '../../shared/auth-state.service';
 import {Title} from "@angular/platform-browser";
+
+//for Google oauth
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import {json} from "express";
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css'],
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent implements OnInit,AfterViewInit {
   loginForm: FormGroup;
   errors:any = null;
   constructor(
@@ -19,7 +25,8 @@ export class SigninComponent implements OnInit {
     public authService: AuthService,
     private token: TokenService,
     private authState: AuthStateService,
-    private titleService:Title
+    private titleService:Title,
+    private zone: NgZone
   ) {
     this.titleService.setTitle("API JWT | Login");
     this.loginForm = this.fb.group({
@@ -27,7 +34,9 @@ export class SigninComponent implements OnInit {
       password: [],
     });
   }
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
   onSubmit() {
     this.authService.signin(this.loginForm.value).subscribe(
       (result) => {
@@ -48,10 +57,9 @@ export class SigninComponent implements OnInit {
     this.token.handleData(data.access_token);
   }
 
-  google_login(){
-    this.authService.google_signin().subscribe(
+  callback_backend(provider:string,data:any){
+    return this.authService.oauth_callback(provider,data).subscribe(
       (result) => {
-        console.log(result);
         this.responseHandler(result);
       },
       (error) => {
@@ -65,20 +73,54 @@ export class SigninComponent implements OnInit {
     );
   }
 
-  github_login(){
-    this.authService.github_signin().subscribe(
-      (result) => {
-        console.log(result);
-        this.responseHandler(result);
-      },
-      (error) => {
-        this.errors = error.error;
-      },
-      () => {
-        this.authState.setAuthState(true);
-        this.loginForm.reset();
-        this.router.navigate(['profile']);
-      }
-    );
+  //for Google oauth
+  auth2: any;
+  @ViewChild('loginRef', {static: true}) loginElement!:ElementRef;
+  ngAfterViewInit() {
+    this.googleAuthSDK();
   }
+
+
+  callLogin() {
+    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+      (googleAuthUser: any) => {
+
+        //Print profile details in the console logs
+        let profile = googleAuthUser.getBasicProfile();
+        // console.log('Token || ' + googleAuthUser.getAuthResponse().id_token);
+        const data=[{'name':profile.getName(),'id':profile.getId(),'email':profile.getEmail()}]
+        this.zone.run(
+          ()=>this.callback_backend('google',JSON.stringify(data))
+        );
+
+      }, (error: any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+
+  }
+
+  googleAuthSDK() {
+
+    (<any>window)['googleSDKLoaded'] = () => {
+      (<any>window)['gapi'].load('auth2', () => {
+        this.auth2 = (<any>window)['gapi'].auth2.init({
+          client_id: '23453012486-7gjvl82hp6qbho6jl03469fgnfiipov1.apps.googleusercontent.com',
+          plugin_name:'login',
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.callLogin();
+      });
+    }
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      // if (d.getElementById(id)) { return; }
+      js = d.createElement('script');
+      js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs?.parentNode?.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+  }
+
 }
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -15,20 +15,21 @@ export class AuthService {
   constructor(private http: HttpClient) {}
   // User registration
   register(user: User): Observable<any> {
+    // return this.http.post('http://localhost:8000/api/register', user);
     return this.http.post('https://secure-api-jwt.herokuapp.com/api/register', user);
   }
   // Login
   signin(user: User): Observable<any> {
+    // return this.http.post<any>('http://localhost:8000/api/login', user);
     return this.http.post<any>('https://secure-api-jwt.herokuapp.com/api/login', user);
   }
-  google_signin(): Observable<any> {
-    return this.http.get<any>('https://secure-api-jwt.herokuapp.com/api/google/redirect');
-  }
-  github_signin(): Observable<any> {
-    return this.http.get<any>('https://secure-api-jwt.herokuapp.com/api/github/redirect');
+  oauth_callback(provider:String,data: any):Observable<any>{
+    // return this.http.get<any>('http://localhost:8000/api/'+provider+'/auth?data='+encodeURIComponent(data));
+    return this.http.get<any>('https://secure-api-jwt.herokuapp.com/api/'+provider+'/auth?data='+encodeURIComponent(data));
   }
   // Access user profile
   profileUser(): Observable<any> {
+    // return this.http.get('http://localhost:8000/api/profile');
     return this.http.get('https://secure-api-jwt.herokuapp.com/api/profile');
   }
 }
